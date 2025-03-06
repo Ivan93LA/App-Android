3d9831b710b5e6b90d37d704b43c7c673a3e25c7
Ivan93LA <ivan.inln17@gmail.com>
2025-03-06 22:08:33 +0100

    Subido

---
+++ app/src/main/java/com/example/androidpruebaivanleon/Variables.kt
@@ -0,0 +1,23 @@
+package com.example.androidpruebaivanleon
+
+//Creo la funcion principal
+
+fun main (){
+    val nombre = "IvanLeon"
+    val edad:Int = 32
+
+    //Existen las variables long, que son mas grandes
+    val ejemplo:Long = 2332123
+
+    val numeroDecimal:Float = 3254.10F
+    // IMPORTANTE poner la "F"
+
+    val NuevoNumero: Double = 23232.2231
+
+    val CharEjemplo1:Char = 'a'
+    val CharEjemplo2:Char = '2'
+
+    val EjemploString:String = "Se puede escribir cualquier frase"
+
+    val EjemploBoolean:Boolean = true
+}
