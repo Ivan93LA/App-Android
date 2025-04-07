67c82c12ae593e5e76b13fdc08da85011c4a176c
Ivan93LA <ivan.inln17@gmail.com>
2025-04-07 22:01:18 +0200

    Apuntes

--- app/src/main/java/com/example/androidpruebaivanleon/Variables.kt
+++ app/src/main/java/com/example/androidpruebaivanleon/Variables.kt
@@ -1,18 +1,10 @@
 package com.example.androidpruebaivanleon
 
 //Creo la funcion principal
-
+val edad:Int = 32
+var edad2:Int = 20
 fun main (){
-    val nombre = "IvanLeon"
-    val edad:Int = 32
 
-    //Existen las variables long, que son mas grandes
-    val ejemplo:Long = 2332123
-
-    val numeroDecimal:Float = 3254.10F
-    // IMPORTANTE poner la "F"
-
-    val NuevoNumero: Double = 23232.2231
 
     val CharEjemplo1:Char = 'a'
     val CharEjemplo2:Char = '2'
@@ -20,4 +12,26 @@ fun main (){
     val EjemploString:String = "Se puede escribir cualquier frase"
 
     val EjemploBoolean:Boolean = true
+
+    println("sumo")
+    println(edad + edad2)
+
+    println("resto")
+    println(edad - edad2)
+
+
+    fun variablesNumericas(){
+        val nombre = "IvanLeon"
+
+        edad2=29
+
+        //Existen las variables long, que son mas grandes
+        val ejemplo:Long = 2332123
+
+        val numeroDecimal:Float = 3254.10F
+        // IMPORTANTE poner la "F"
+
+        val NuevoNumero: Double = 23232.2231
+
+    }
 }
